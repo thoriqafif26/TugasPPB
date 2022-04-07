721dc000505665898435ce04f585e6fd2e2547a2
thoriqafif26 <98322760+thoriqafif26@users.noreply.github.com>
2022-04-07 23:47:47 +0700

    Tugas 3 PPB

---
+++ app/src/main/java/com/moh/thoriq_afif_19102153/helloworld/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.moh.thoriq_afif_19102153.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ lifecycleapp/src/main/java/com/moh/thoriq_afif_19102153/lifecycleapp/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.moh.thoriq_afif_19102153.lifecycleapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btnPage: Button = findViewById(R.id.btnPage)
+        btnPage.setOnClickListener{
+            val intent = Intent(this, HalamanDuaActivity::class.java)
+            startActivity(intent)
+        }
+    }
+    fun printState(msg: String) {
+        Log.d("ActivityState", msg)
+        Toast.makeText(applicationContext, msg, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        printState("Halaman satu On Start")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        printState("Halaman satu On Resume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        printState("Halaman satu On Pause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        printState("Halaman satu On Stop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        printState("Halaman satu On Restart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        printState("Halaman satu On Destroy")
+    }
+}

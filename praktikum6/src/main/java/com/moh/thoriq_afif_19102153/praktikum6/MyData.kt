697681f5c38020dbf11f352d08da7b0db4e84ca1
thoriqafif26 <98322760+thoriqafif26@users.noreply.github.com>
2022-04-27 15:40:06 +0700

    praktikum 6

---
+++ praktikum6/src/main/java/com/moh/thoriq_afif_19102153/praktikum6/MyData.kt
@@ -0,0 +1,12 @@
+package com.moh.thoriq_afif_19102153.praktikum6
+
+import android.os.Parcelable
+import android.provider.MediaStore
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class MyData(
+    var name: String,
+    var description: String,
+    var photo: String
+) : Parcelable

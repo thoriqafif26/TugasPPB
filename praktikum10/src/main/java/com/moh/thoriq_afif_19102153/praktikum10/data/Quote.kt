5853331aed802795f28688ba4e81f19a001cd083
thoriqafif26 <98322760+thoriqafif26@users.noreply.github.com>
2022-06-22 09:09:36 +0700

    praktikum 10

---
+++ praktikum10/src/main/java/com/moh/thoriq_afif_19102153/praktikum10/data/Quote.kt
@@ -0,0 +1,13 @@
+package com.moh.thoriq_afif_19102153.praktikum10.data
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Quote(
+    var id: Int = 0,
+    var title: String? = null,
+    var description: String? = null,
+    var category: String? = null,
+    var date: String? = null
+) : Parcelable
